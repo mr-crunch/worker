a8aa4493735fb9f1d5b0a99cebc2ba92235ae2e3
mr-crunch <mattbianco4@gmail.com>
2025-02-18 14:19:21 -0600

    full rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+use std::collections::HashMap;
 use std::io;
+
+struct Worker {
+    name: String,
+    department: String,
+    position: String,
+    salary: String,
+}
+
+impl Worker {
+    fn print(&self) {
+        println!(
+            "name: {} department: {}, position: {} salary: {}",
+            self.name, self.department, self.position, self.salary
+        );
+    }
+}
+
 fn main() {
     loop {
         let mut input = String::new();
@@ -7,18 +25,33 @@ fn main() {
             Ok(string) => input = string,
             Err(error) => eprintln!("error: {:?}", error),
         }
-        if input == "exit" {
+        let names = input.split_whitespace().collect::<Vec<&str>>();
+        if names[0] == "exit" {
             break;
-        } else {
-            let names = input.split_whitespace().collect::<Vec<&str>>();
-            println!("input: {:?}", names);
+        }
+        let employees = employee_list(names);
+        for employee in employees {
+            employee.1.print()
         }
     }
 }
 
 fn get_input() -> Result<String, io::Error> {
-    println!("enter input: ");
+    println!("enter names: ");
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
     Ok(input.trim().to_string())
+}
+
+fn employee_list(input: Vec<&str>) -> HashMap<String, Worker> {
+    let mut employees = HashMap::new();
+    for name in input {
+        employees.entry(name.to_string()).or_insert(Worker {
+            name: name.to_string(),
+            department: String::from("na"),
+            position: String::from("na"),
+            salary: String::from("na"),
+        });
+    }
+    employees
 }
